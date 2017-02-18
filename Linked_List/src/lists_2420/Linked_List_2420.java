a78a3b582923ef9e5c5bd1bcbac04f0958c53d4a
Mark Van der Merwe <vandermerwem2@gmail.com>
2017-02-18 16:51:09 -0700

    Test cases done.

--- Linked_List/src/lists_2420/Linked_List_2420.java
+++ Linked_List/src/lists_2420/Linked_List_2420.java
@@ -6,22 +6,22 @@ package lists_2420;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
-public class Linked_List_2420 implements List_2420{
+public class Linked_List_2420<Type> implements List_2420<Type>{
 
 	@Override
-	public void add_first(Object data) {
+	public void add_first(Type data) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void add_last(Object data) {
+	public void add_last(Type data) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void add_middle(int after, Object data) {
+	public void add_middle(int after, Type data) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -33,37 +33,37 @@ public class Linked_List_2420 implements List_2420{
 	}
 
 	@Override
-	public boolean contains(Object item) {
+	public boolean contains(Type item) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean contains_recursive(Object item) {
+	public boolean contains_recursive(Type item) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public Object get_first() throws NoSuchElementException {
+	public Type get_first() throws NoSuchElementException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Object get_last() throws NoSuchElementException {
+	public Type get_last() throws NoSuchElementException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Object remove_first() throws NoSuchElementException {
+	public Type remove_first() throws NoSuchElementException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Object remove_last() throws NoSuchElementException {
+	public Type remove_last() throws NoSuchElementException {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -87,97 +87,17 @@ public class Linked_List_2420 implements List_2420{
 	}
 
 	@Override
-	public ArrayList to_ArrayList_post_recurse() {
+	public ArrayList<Type> to_ArrayList_post_recurse() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public ArrayList to_ArrayList() {
+	public ArrayList<Type> to_ArrayList() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	/**
-	 * FIXME: comments
-	 *
-	 * Pictorially, a node is:
-	 *
-	 *  data  next
-	 *  ---------- 
-	 *  | 5  |---+--->
-	 *  ---------- 
-	 *  
-	 *  Note, while a 5 is used above any "Type" could be contained in the node
-	 */
-	static class Node<Type>
-	{
-	  //FIXME: create a data element of the Given Type
-	  //FIXME: create reference to another node
-	  
-	  //FIXME: write a constructor that simplifies building an initial node
-	  public Node( Type the_data, Node<Type> after_me) {
-		  
-	  }
-	  
-	  /**
-	   * 
-	   * This function must be written recursively.
-	   * 
-	   * @return the length of this "chain of nodes", including self.
-	   *         
-	   *  Note: 1) it doesn't matter if something (or multiple somethings) points to this node, 
-	   *        2) if this node doesn't point at anything, then the size would be 1.
-	   */
-	  int length() {
-		  return -1;
-	  }
-	  
-	  /**
-	   * recursive determine if the item is in this node or the nodes after
-	   * 
-	   * @param item
-	   *            - needle
-	   * @return true if item in chain
-	   */
-	   boolean contains_recursive( Type item ){
-		   return false;
-	   }
-			
 
-	  /**
-	   * This function must be written recursively (using a helper method, with the
-	   * arraylist as a parameter, to do the recursion)
-	   * 
-	   * Create an array list containing the data from this node and all nodes after it. 
-	   * 
-	   * In the helper method, add the data to the array list after the recursive call,
-	   * thus "reversing" the list.
-	   *
-	   */
-	  ArrayList<Type> to_ArrayList_post_recursive() {
-		  return null;
-	  }
-
-	  /**
-	   *
-	   * FIXME: this method must be implemented using recursion
-	   *
-	   * Creates a string that describes the current node and all following nodes, for
-	   * example, a list of the nubmers 0, 1, 2, 3 would print as:
-	   * 
-	   * "[0]--> [1]--> [2]--> [3]--> null"
-	   *
-	   * WARNING: the syntax must be exact.  
-	   *          "open square bracket, data, close square bracket, hyphen, hyphen, greater than space,
-	   *          ... null"
-	   *
-	   * @return a string representation of this chain of nodes
-	   */
-	  public String toString() {
-		  return "";
-	  }
-	}
 
 
 }
--- Linked_List/src/lists_2420/List_2420Test.java
+++ Linked_List/src/lists_2420/List_2420Test.java
@@ -5,13 +5,233 @@ package lists_2420;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class List_2420Test {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private List_2420<Integer> sampleList;
+
+	/**
+	 * allow changing between linked and array lists
+	 */
+	public List_2420<Integer> new_list() {
+		return new Linked_List_2420<Integer>();
+		// return new Array_List_2420();
 	}
 
+	@Before
+	public void setUp() {
+		sampleList = new_list();
+		sampleList.add_first(3);
+		sampleList.add_first(2);
+		sampleList.add_first(5);
+	}
+
+	/**
+	 * Test adding val to front of list.
+	 */
+	@Test
+	public void testAddFirst() {
+		List_2420<Integer> newList = new_list();
+		newList.add_first(5);
+		try {
+			Integer firstVal = newList.get_first();
+			assertTrue(5 == firstVal);
+		} catch (NoSuchElementException e) {
+			fail("get_first threw an exception");
+		}
+	}
+
+	/**
+	 * Test adding val to end of list.
+	 */
+	@Test
+	public void testAddLast() {
+		sampleList.add_last(5);
+		try {
+			Integer lastVal = sampleList.get_last();
+			assertTrue(5 == lastVal);
+		} catch (NoSuchElementException e) {
+			fail("get_last threw an exception");
+		}
+	}
+
+	/**
+	 * Test clearing our list.
+	 */
+	@Test
+	public void testClear() {
+		sampleList.clear();
+		try {
+			sampleList.get_first();
+			// If we reach here, val exists so we fail.
+			fail("Value found in empty array.");
+		} catch (NoSuchElementException e) {
+			// Test passes.
+		}
+
+	}
+
+	/**
+	 * Test if we can tell if val exists in list.
+	 */
+	@Test
+	public void testContains() {
+		assertTrue(sampleList.contains(2));
+		assertTrue(sampleList.contains(3));
+		assertTrue(sampleList.contains(5));
+		assertFalse(sampleList.contains(9));
+	}
+
+	/**
+	 * Test if we can tell if val exists in list using recursion. Does the same
+	 * as testsContains because functionality should be identical.
+	 */
+	@Test
+	public void testContainsRecursive() {
+		if (sampleList instanceof Linked_List_2420) {
+			assertTrue(sampleList.contains_recursive(2));
+			assertTrue(sampleList.contains_recursive(3));
+			assertTrue(sampleList.contains_recursive(5));
+			assertFalse(sampleList.contains_recursive(9));
+		}
+	}
+
+	/**
+	 * Test that we can get the first value in a list.
+	 */
+	@Test
+	public void testGetFirst() {
+		Integer firstVal = sampleList.get_first();
+		assertTrue(5 == firstVal);
+
+		List_2420<Integer> emptyList = new_list();
+		try {
+			emptyList.get_first();
+			// If we reach here, val exists so we fail.
+			fail("Value found in empty array.");
+		} catch (NoSuchElementException e) {
+			// Test passes.
+		}
+	}
+
+	/**
+	 * Test that we can get the last val in a list.
+	 */
+	@Test
+	public void testGetLast() {
+		Integer lastVal = sampleList.get_last();
+		assertTrue(3 == lastVal);
+
+		List_2420<Integer> emptyList = new_list();
+		try {
+			emptyList.get_last();
+			// If we reach here, val exists so we fail.
+			fail("Value found in empty array.");
+		} catch (NoSuchElementException e) {
+			// Test passes.
+		}
+	}
+
+	/**
+	 * Test that we can remove the first value in a list.
+	 */
+	@Test
+	public void testRemoveFirst() {
+		Integer firstVal = sampleList.remove_first();
+		assertTrue(5 == firstVal);
+		assertEquals(2, sampleList.size());
+
+		List_2420<Integer> emptyList = new_list();
+		try {
+			emptyList.remove_first();
+			// If we reach here, val exists so we fail.
+			fail("Value found in empty array.");
+		} catch (NoSuchElementException e) {
+			// Test passes.
+		}
+	}
+
+	/**
+	 * Test that we can remove the last value in a list.
+	 */
+	@Test
+	public void testRemoveLast() {
+		Integer lastVal = sampleList.remove_last();
+		assertTrue(3 == lastVal);
+		assertEquals(2, sampleList.size());
+
+		List_2420<Integer> emptyList = new_list();
+		try {
+			emptyList.remove_last();
+			// If we reach here, val exists so we fail.
+			fail("Value found in empty array.");
+		} catch (NoSuchElementException e) {
+			// Test passes.
+		}
+	}
+
+	/**
+	 * Test getting the size of our list.
+	 */
+	@Test
+	public void testGetSize() {
+		assertEquals(3, sampleList.size());
+
+		assertEquals(0, new_list().size());
+	}
+
+	/**
+	 * Test getting the size of our list recursively.
+	 */
+	@Test
+	public void testComputeSizeRecursive() {
+		if (sampleList instanceof Linked_List_2420) {
+			assertEquals(3, sampleList.compute_size_recursive());
+
+			assertEquals(0, new_list().compute_size_recursive());
+		}
+	}
+
+	/**
+	 * Make sure we can reverse the vals in our list.
+	 */
+	@Test
+	public void testReverse() {
+		sampleList.reverse();
+		try {
+			assertTrue(3 == sampleList.get_first());
+			assertTrue(5 == sampleList.get_last());
+		} catch (NoSuchElementException e) {
+			fail("No element found.");
+		}
+	}
+
+	/**
+	 * Turn list into ArrayList of reverse order.
+	 */
+	@Test
+	public void testToArrayListPostRecurse() {
+		if(sampleList instanceof Linked_List_2420) {
+			ArrayList<Integer> reversedData = sampleList.to_ArrayList_post_recurse();
+			assertTrue(3 == reversedData.get(0));
+			assertTrue(2 == reversedData.get(1));
+			assertTrue(5 == reversedData.get(2));
+		}
+	}
+	
+	/**
+	 * Turns list into ArrayList.
+	 */
+	@Test
+	public void testToArrayList() {
+		ArrayList<Integer> reversedData = sampleList.to_ArrayList();
+		assertTrue(5 == reversedData.get(0));
+		assertTrue(2 == reversedData.get(1));
+		assertTrue(3 == reversedData.get(2));
+	}
 }
