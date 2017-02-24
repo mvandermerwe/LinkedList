28f6e58c0a4049182c12f3a4cd570b12029399d8
JJ Garzella <jjgarzella@gmail.com>
2017-02-23 23:17:49 -0700

    Documentation, code fixes

--- Linked_List/src/lists_2420/Timing.java
+++ Linked_List/src/lists_2420/Timing.java
@@ -9,6 +9,11 @@ import java.util.Random;
 
 public class Timing {
 
+	/**
+	 * This enum allows the timing code to be paramaterized
+	 * @author jjgarzella
+	 *
+	 */
 	static enum InsertType {
 		BEGINNING, MIDDLE, END, REMOVEBEGIN, REMOVEEND;
 
@@ -31,43 +36,94 @@ public class Timing {
 		}
 	}
 
+	/**
+	 * The number of times each test will be run, and averaged.
+	 */
 	private static int NUM_ITERATIONS = 100;
+	
+	/**
+	 * Random generator for adding into the middle of an array
+	 */
 	private static Random generator = new Random();
+	
+	/**
+	 * The values of N which will be run by a test
+	 */
 	private static final Integer[] N_VALUES = { 400_000, 800_000, 1_200_000, 1_600_000, 2_000_000, 2_400_000, 2_800_000,
 			3_200_000, 3_600_000, 4_000_000 };
 
+	/**
+	 * Test for adding to the front of the list
+	 * 
+	 * @param list 
+	 */
 	public static void insertIntoBeginning(List_2420<Integer> list) {
 		for (int counter = 0; counter < NUM_ITERATIONS; counter++) {
 			list.add_first(1);
 		}
 	}
 
+	/**
+	 * Test for removing from the front of the list; does not iterate
+	 * 
+	 * @param list
+	 */
 	public static void removeFromBeginning(List_2420<Integer> list) {
 		list.remove_first();
 	}
 
+	/**
+	 * Add NUM_ITERATIONS elements to index N in the list
+	 * Used to test adding into the middle
+	 * 
+	 * @param list
+	 * @param index
+	 */
 	public static void insertIntoMiddle(List_2420<Integer> list, int index) {
 		for (int counter = 0; counter < NUM_ITERATIONS; counter++) {
 			list.add_middle(index, 1);
 		}
 	}
 
+	/**
+	 * Test for adding to the end of the list
+	 * 
+	 * @param list
+	 */
 	public static void insertAtEnd(List_2420<Integer> list) {
 		for (int counter = 0; counter < NUM_ITERATIONS; counter++) {
 			list.add_last(1);
 		}
 	}
 
+	/**
+	 * Test for removing from the end of a list
+	 * 
+	 * @param list
+	 */
 	public static void removeFromEnd(List_2420<Integer> list) {
 		list.remove_last();
 	}
 
+	/**
+	 * Creates an array with numberOfElment items, all populated with 1
+	 * 
+	 * @param list
+	 * @param numberOfElements
+	 */
 	public static void createArray(List_2420<Integer> list, int numberOfElements) {
 		for (int counter = 0; counter < numberOfElements; counter++) {
 			list.add_last(1);
 		}
 	}
 
+	/**
+	 * Tests and times one type of List timing code. Writes output to a csv
+	 * 
+	 * @param listClass the class of the array to be tested. Must be a List_2420
+	 * @param methodType the type of test to run
+	 * @param fileSuffix a short string to be added to the name of the csv file
+	 */
 	public static void testInsertMethod(Class<?> listClass, InsertType methodType, String fileSuffix) {
 
 		// use a string builder, so we only have to write to file once.
@@ -82,11 +138,6 @@ public class Timing {
 		} else {
 			throw new IllegalArgumentException("The listClass paramater must be a List_2420");
 		}
-
-		// in add_middle, the random generator for the index
-		// cannot generate a number between 0 and 0. Add one
-		// element to the array to avoid this
-		// testArray.add_first(1);
 
 		// perform the test for all specified values of N in the array N_VALUES
 		for (int numElements : N_VALUES) {
@@ -128,8 +179,12 @@ public class Timing {
 				throw new RuntimeException("Unknown case of InsertType inner enum");
 			}
 
-			double elapsedSeconds = ((end - start) / ((double) 1_000_000_000)) / NUM_ITERATIONS;
+			double elapsedSeconds = ((end - start) / ((double) 1_000_000_000));
 
+			if (!(methodType.equals(InsertType.REMOVEBEGIN) || methodType.equals(InsertType.REMOVEEND))) {
+				elapsedSeconds /= 100;
+			}
+			
 			fileData.append(numElements + "," + elapsedSeconds + "\n");
 		}
 
@@ -147,15 +202,17 @@ public class Timing {
 
 	public static void main(String[] args) {
 
-		String suffix = "norand_2";
+		String suffix = "sample";
 
-//		testInsertMethod(Linked_List_2420.class, InsertType.BEGINNING, suffix);
-//		testInsertMethod(Linked_List_2420.class, InsertType.MIDDLE, suffix);
-//		testInsertMethod(Linked_List_2420.class, InsertType.END, suffix);
-//
-//		testInsertMethod(Array_List_2420.class, InsertType.BEGINNING, suffix);
-//		testInsertMethod(Array_List_2420.class, InsertType.MIDDLE, suffix);
-//		testInsertMethod(Array_List_2420.class, InsertType.END, suffix);
+		// You may comment out some of these in order to only run the tests you want.
+		
+		testInsertMethod(Linked_List_2420.class, InsertType.BEGINNING, suffix);
+		testInsertMethod(Linked_List_2420.class, InsertType.MIDDLE, suffix);
+		testInsertMethod(Linked_List_2420.class, InsertType.END, suffix);
+
+		testInsertMethod(Array_List_2420.class, InsertType.BEGINNING, suffix);
+		testInsertMethod(Array_List_2420.class, InsertType.MIDDLE, suffix);
+		testInsertMethod(Array_List_2420.class, InsertType.END, suffix);
 		
 		testInsertMethod(Linked_List_2420.class, InsertType.REMOVEBEGIN, suffix);
 		testInsertMethod(Linked_List_2420.class, InsertType.REMOVEEND, suffix);
